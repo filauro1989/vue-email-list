c30d149647996aaba49c2874244170e53d75d0c5
filauro1989 <filauro1989@gmail.com>
2022-01-10 15:58:15 +0100

    generate and print emails

--- script.js
+++ script.js
@@ -1,51 +1,26 @@
 const app = new Vue({
     el: "#app",
     data: {
-    number: null,
+        email: [],
     }, 
     created() {
-    const self = this;
-      // axios
-      //   .get("https://flynn.boolean.careers/exercises/api/random/int")
-      //   .then(function (response) {
-      //     // handle success
-      //     console.log(response);
-      //     console.log(self.number);
-      //     self.number = response.data.response;
-      //   })
-      //   .catch(function (error) {
-      //     // handle error
-      //     console.log(error);
-      //   });
-
-      // axios
-      //   .get("https://flynn.boolean.careers/exercises/api/random/sentence")
-      //   .then((response) => {
-      //     console.log(response);
-      //     console.log(this);
-      //   })
-      //   .catch((error) => {
-      //     console.log(error);
-      //   });
-
-      //https://flynn.boolean.careers/exercises/api/array/integers? url
-      //min=n&max=n&items=n parametri
-
-    axios
-        .get("https://flynn.boolean.careers/exercises/api/array/integers", {
-        params: {
-            min: 2,
-            max: 100,
-            items: 10
-        },
-        })
-        .then(function (response) {
-        console.log(response);
-        })
-        .catch(function (error) {
-        console.log(error);
-        });
-    
+        this.createMails()
+        
     },
-    methods: {}
+    methods: {
+        createMails() {
+            for (let index = 0; index < 10; index++) {
+                const self = this;
+                axios
+                    .get("https://flynn.boolean.careers/exercises/api/random/mail")
+                    .then(function (response) {
+                    self.email.push(response.data.response);
+                    console.log(self.email);
+                    })
+                    .catch(function (error) {
+                    console.log(error);
+                    });
+            }
+        }
+    }
 });
